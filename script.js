0ab538505956abc1aa78cc739fa1693aac9b7be6
Noami Costa <noamicafft@gmail.com>
2022-03-19 17:44:22 +0000

    last changes

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-(function(){
-    let articles = document.getElementsByName("article");
 
-    for(let i = 0; i < articles.length; i++){
-        articles[i].addEventListener("click", function(evt){
-            alert(i);
-        });
-    }
-}())
+window.onload = function() {
+
+
+    (function(){
+        let copyright = document.getElementById("copyright");
+        let date = new Date(Date.now()).getFullYear();
+    
+        copyright.innerText = date;
+    }())
+    
+    
+    (function(){
+        let skills = document.getElementsByClassName("skills");
+    
+        for(let i = 0; i < skills.length; i++){
+            skills[i].style.position = "relative";
+            skills[i].addEventListener("mouseover", function(evt){
+                
+               let p =  document.createElement("div");
+               p.innerText = "empty yet";
+               p.style.fontSize = "0.6em";
+               p.style.color = "#E60965";
+               p.style.opacity = "0.6";
+               p.style.position = "absolute";
+               p.style.width = "100%";
+               p.style.right = "0";
+               p.style.bottom = "0";
+               skills[i].appendChild(p)
+            });
+
+            skills[i].addEventListener("mouseout", function(evt){
+                let child = skills[i].lastElementChild
+                while(child){
+                    skills[i].removeChild(child)
+                    child = skills[i].lastElementChild
+                }
+                 
+            });
+        }
+    }())
+
+}
+
+
 
